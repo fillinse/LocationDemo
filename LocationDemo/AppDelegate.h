930754a538c2c9bdbcaa28d6459eaca2e8c92b3c
fillinse <Fillinse@192.168.6.56>
2015-12-21 15:37:51 +0800

    Initial Commit

---
+++ LocationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationDemo
+//
+//  Created by Fillinse on 15/12/21.
+//  Copyright © 2015年 Fillinse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
